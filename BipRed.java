eebd106d579b8f954747aaac33dafda51e4a5e21
katja7667 <katja7667@gmail.com>
2014-11-06 22:07:04 +0100

    added start files

---
+++ BipRed.java
@@ -0,0 +1,127 @@
+/**
+ * Exempel på in- och utdatahantering för maxflödeslabben i kursen
+ * ADK.
+ *
+ * Använder Kattio.java för in- och utläsning.
+ * Se http://kattis.csc.kth.se/doc/javaio
+ *
+ * @author: Per Austrin
+ */
+
+public class BipRed {
+    Kattio io;
+    int x;
+    int y;
+    int e;
+	int[][] allEdges;
+
+    void readBipartiteGraph() {
+	// Läs antal hörn och kanter
+	x = io.getInt();
+	y = io.getInt();
+	e = io.getInt();
+	
+	allEdges = new int[2][e];
+
+	// Läs in kanterna
+	for (int i = 0; i < e; ++i) {
+	    allEdges[0][i] = io.getInt();
+	    allEdges[1][i] = io.getInt();
+	}
+    }
+    
+    
+    void writeFlowGraph(int v, int e, int[][] allEdges) {
+	int v = x + y; 
+	int s = v + 1; 
+	int t = v + 2;
+	int c = 1;
+	// Skriv ut antal hörn och kanter samt källa och sänka
+	io.println(x+y+2);
+	io.println(s + " " + t);
+	io.println(e+x+y);
+	for(int i = 1; i <= x; i++){
+		io.println(s + " " + i + " " + c);
+	}
+	for (int i = 0; i < e; ++i) {
+	    io.println(allEdges[0][i] + " " + allEdges[1][i] + " " + c);
+	}
+	for(int i = x+1; i <= y; i++){
+		io.println(i + " " + t + " "+ c);
+	}
+
+	// Var noggrann med att flusha utdata när flödesgrafen skrivits ut!
+	io.flush();
+	
+	// Debugutskrift
+	System.err.println("Skickade iväg flödesgrafen");
+    }
+    
+    
+    void readMaxFlowSolution() {
+	// Läs in antal hörn, kanter, källa, sänka, och totalt flöde
+	// (Antal hörn, källa och sänka borde vara samma som vi i grafen vi
+	// skickade iväg)
+	int v = io.getInt();
+	int s = io.getInt();
+	int t = io.getInt();
+	int totflow = io.getInt();
+	int e = io.getInt();
+
+	allEdges = new int[3][e];
+	
+	io.println(x+" "+y);
+	io.println(totflow);
+
+	for (int i = 0; i < e; ++i) {
+	    // Flöde f från a till b
+	    allEdges[0][i] = io.getInt();
+	    allEdges[1][i] = io.getInt();
+	    allEdges[2][i] = io.getInt();
+	   
+	    if(allEdges[0][i] <s||allEdges[1][i]<s)
+	    	io.println(allEdges[0][i]+" "+allEdges[1][i]);
+	}
+
+
+    }
+    
+    
+    void writeBipMatchSolution() {
+	int x = 17, y = 4711, maxMatch = 0;
+	
+	// Skriv ut antal hörn och storleken på matchningen
+	io.println(x + " " + y);
+	io.println(maxMatch);
+	
+	for (int i = 0; i < maxMatch; ++i) {
+	    int a = 5, b = 2323;
+	    // Kant mellan a och b ingår i vår matchningslösning
+	    io.println(a + " " + b);
+	}
+	
+    }
+    
+    BipRed() {
+	io = new Kattio(System.in, System.out);
+	
+	readBipartiteGraph();
+	
+	writeFlowGraph();
+	
+	readMaxFlowSolution();
+	
+	writeBipMatchSolution();
+
+	// debugutskrift
+	System.err.println("Bipred avslutar\n");
+
+	// Kom ihåg att stänga ner Kattio-klassen
+	io.close();
+    }
+    
+    public static void main(String args[]) {
+	new BipRed();
+    }
+}
+
---
+++ maxFlow.java
@@ -0,0 +1,20 @@
+Nu ska du skriva ett program som löser flödesproblemet. Programmet ska läsa 
+indata från standard input och skriva lösningen till standard output. Se nedan 
+hur in- och utdataformaten för flödesproblemet ser ut.
+Ditt program ska lösa problemet effektivt. Kattis kommer att provköra programmet 
+på generella flödesgrafer på upp till 2000 hörn och 10000 kanter. Kattis känner 
+till problemet som oldkattis:adkmaxflow.
+
+läsa
+
+int v = io.getInt();
+int s = io.getInt();
+int t = io.getInt();
+int e = io.getInt();
+
+int 
+
+for(int i = 0; i < e; i++ ){
+	spara på ett bra sätt för att sedan 
+	gå igenom alla möjliga vägar. 
+}
