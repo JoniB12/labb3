28a88fc58a295311e5b172d36a0c759e545cd820
katja7667 <katja7667@gmail.com>
2014-11-07 11:11:49 +0100

    maxFlow now makes neighbor list and capacity matrix

--- maxFlow.java
+++ maxFlow.java
@@ -1,20 +1,35 @@
-Nu ska du skriva ett program som löser flödesproblemet. Programmet ska läsa 
-indata från standard input och skriva lösningen till standard output. Se nedan 
-hur in- och utdataformaten för flödesproblemet ser ut.
-Ditt program ska lösa problemet effektivt. Kattis kommer att provköra programmet 
-på generella flödesgrafer på upp till 2000 hörn och 10000 kanter. Kattis känner 
-till problemet som oldkattis:adkmaxflow.
+/**
+*
+*
+*@autor: Sara F och Katja R
+*/
 
-läsa
+public class maxFlox {
+	int v = io.getInt();
+	int s = io.getInt();
+	int t = io.getInt();
+	int e = io.getInt();
+	
+	int vertex = io.getInt();
+	int neighbor;
+	int capacity;
 
-int v = io.getInt();
-int s = io.getInt();
-int t = io.getInt();
-int e = io.getInt();
+	List<int[]> neighborList = new List<int[]> neighborList(e); 
+	int[][] capacityMatrix = new int[][] capacityMatrix();
+	
+	// Bygger grannlista och capacitetsmatris
+	for(int i = vertex; i < v; i++ ){
+		int vertexTmp = vertex;
+		while(vertexTmp == vertex){
+			neighbor = io.getInt()
+			neighborList(i).add(neighbor);
+			capacity = io.getInt();
+			capacityMatrix[i][neighbor] = capacity;
+			vertex = io.getInt();
+		}	
+	}
 
-int 
+	void edmondsKarp(){
 
-for(int i = 0; i < e; i++ ){
-	spara på ett bra sätt för att sedan 
-	gå igenom alla möjliga vägar. 
+	}
 }
