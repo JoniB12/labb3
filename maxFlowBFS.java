1b2272f0705af090bafda9d309291ba9b60e89d5
Sara Feychting <sara.feychting@gmail.com>
2014-12-15 01:58:35 +0100

    jag vill sova

--- maxFlowBFS.java
+++ maxFlowBFS.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.lang.Math;
 
 public class maxFlowBFS {
@@ -27,40 +29,14 @@ public class maxFlowBFS {
     t = io.getInt();
     e = io.getInt();
     edges = new int[v+1][v+1];
-    for (int i = 0; i < edges.length; i++) {
-      Arrays.fill(edges[i], 0);
-    }
     for (int i = 0; i < e; i++) {
       int x = io.getInt();
       int y = io.getInt();
       int c = io.getInt();
-      edges[x][y] += c;
+      edges[x][y] = c;
     }
   }
 
-  // private void solveFlow() {
-  //   boolean[] visited;
-  //   int flow = -1;
-  //   totFlow = 0;
-  //   while (flow != 0) {
-  //     visited = new boolean[v+1];
-  //     visited[s] = true;
-  //     flow = recursion(visited, s, 0);
-  //     totFlow += flow;
-  //   }
-  // }
-
-  // fixed, might actually work
-  // private int getNextEdge(boolean[] visited, int x) {
-  //   for (int i = 0; i < v+1; i++) {
-  //     Edge curEdge = edgeX.get(i);
-  //     if (curEdge.getCap()>0 && !visited[curEdge.getEnd()]) {
-  //       visited[curEdge.getEnd()] = true;
-  //       return curEdge;
-  //     }
-  //   }
-  //   return null;
-  // }
 
   private int bfs() {
     F = new int[v+1][v+1];
@@ -70,8 +46,8 @@ public class maxFlowBFS {
       path[s] = s;
       maxflow = new int[v+1];
       maxflow[s] = Integer.MAX_VALUE;
-      Queue<Integer> fifo = new LinkedList<Integer>();
-      fifo.offer(s);
+      ArrayDeque<Integer> fifo = new ArrayDeque<Integer>();
+      fifo.add(s);
       while (!fifo.isEmpty()) {
         int vertex = fifo.poll();
         for (int i = 1; i < edges[vertex].length; i++) {
@@ -79,7 +55,7 @@ public class maxFlowBFS {
             path[i] = vertex;
             maxflow[i] = Math.min(maxflow[vertex], edges[vertex][i] - F[vertex][i]);
             if (i != t) {
-              fifo.offer(i);
+              fifo.add(i);
             }
             else {
               while (path[i] != i) {
@@ -111,8 +87,8 @@ public class maxFlowBFS {
     for (int i = 0; i < e; i++) {
       int[] edge = matrix.get(i);
       io.println(edge[0] + " " + edge[1] + " " + edge[2]);
-      io.flush();
     }
+    io.flush();
   }
 
   private ArrayList<int[]> getMatrix() {
